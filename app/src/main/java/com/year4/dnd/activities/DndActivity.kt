d22d6c481066fe0432bd3def6fe9d71ab532829f
Jack Noonan <jackpersonal2018@gmail.com>
2023-03-04 02:31:50 +0000

    moved dndactivity to its own package, made model package and made use of this new model in DndActivity.kt

--- app/src/main/java/com/year4/dnd/activities/DndActivity.kt
+++ app/src/main/java/com/year4/dnd/activities/DndActivity.kt
@@ -1,17 +1,21 @@
-package com.year4.dnd
+package com.year4.dnd.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
+import com.year4.dnd.R
 import com.year4.dnd.databinding.ActivityDndBinding
+import com.year4.dnd.models.DndModel
 import timber.log.Timber
 import timber.log.Timber.i
 
 class DndActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityDndBinding
+    var dnd = DndModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dnd)
 
         binding = ActivityDndBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -21,17 +25,15 @@ class DndActivity : AppCompatActivity() {
         i("Dnd Activity started")
 
         binding.btnAdd.setOnClickListener() {
-            val dndTitle = binding.dndTitle.text.toString()
-            if (dndTitle.isNotEmpty()) {
-                i("add Button Pressed: $dndTitle")
+            dnd.title = binding.dndTitle.text.toString()
+            if (dnd.title.isNotEmpty()) {
+                i("add Button Pressed: $dnd.title")
             }
             else {
                 Snackbar
-                    .make(it,"Please Enter a Name", Snackbar.LENGTH_LONG)
+                    .make(it,"Please Enter a name", Snackbar.LENGTH_LONG)
                     .show()
             }
         }
-
-
     }
 }
---
+++ app/src/main/java/com/year4/dnd/models/Dndmodel.kt
@@ -0,0 +1,6 @@
+package com.year4.dnd.models
+
+data class DndModel(var title: String = "")
+
+
+
